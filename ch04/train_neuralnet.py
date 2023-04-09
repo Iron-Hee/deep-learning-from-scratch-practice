896639dc407363b40d33dd624c9b81e996d89266
Iron-Hee <maks012199@naver.com>
2023-04-09 22:08:56 +0900

    chapter 5 구현, dot 연산 에러 아직 못 잡음

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -8,12 +8,16 @@ from two_layer_net import TwoLayerNet
   load_mnist(normalize=True, one_hot_label=True)
   
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
 network = TwoLayerNet(input_size = 784, hidden_size = 50, output_size = 10)
+
+iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
   batch_mask = np.random.choice(train_size, batch_size)
@@ -27,3 +31,10 @@ for i in range(iters_num):
     
   loss = network.loss(x_batch, t_batch)
   train_loss_list.append(loss)
+  
+  if i % iter_per_epoch == 0:
+    train_acc = network.accuracy(x_train, t_train)
+    test_acc = network.accuracy(x_test, t_test)
+    train_acc_list.append(train_acc)
+    test_acc_list.append(test_acc)
+    print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
